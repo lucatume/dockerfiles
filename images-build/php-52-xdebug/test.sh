29c98e9e93b799f6b0e849ddca2b8d929eb57827
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-28 16:15:08 +0200

    make sure Xdebug, Opcache and Memcache can be configured

--- images-build/php-52-xdebug/test.sh
+++ images-build/php-52-xdebug/test.sh
@@ -6,3 +6,9 @@ dgoss run sut
 dgoss run -e XDEBUG_REMOTE_HOST=1.2.3.4 sut
 dgoss run -e XDEBUG_REMOTE_PORT=8877 sut
 dgoss run -e XDEBUG_REMOTE_PORT=8877 -e XDEBUG_REMOTE_HOST=1.2.3.4 sut
+dgoss run -e XDEBUG_DISABLE=1 sut
+dgoss run -e XDEBUG_DISABLE=1-e XDEBUG_REMOTE_HOST=1.2.3.4 sut
+dgoss run -e XDEBUG_DISABLE=1-e XDEBUG_REMOTE_PORT=8877 sut
+dgoss run -e XDEBUG_DISABLE=1 -e XDEBUG_REMOTE_PORT=8877 -e XDEBUG_REMOTE_HOST=1.2.3.4 sut
+dgoss run -e OPCACHE_DISABLE=1 sut
+dgoss run -e MEMCACHE_DISABLE=1 sut
