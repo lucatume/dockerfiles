672fccfd28187f10eca89af3f068b2fc7157eabd
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-29 10:42:43 +0200

    rename function to start/stop php support container

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -4,22 +4,22 @@
 docker pull php:7.1-fpm-alpine
 
 # spin up the required PHP container
-function stop_support_container(){
+function stop_php_container(){
     running="$(docker ps -aq --filter="name=test_support_php")"
     if [[ $running != "" ]]; then
         docker stop $running
     fi
 }
 
-function start_support_container(){
-    stop_support_container
+function start_php_container(){
+    stop_php_container
     docker run --name test_support_php --rm -d php:7.1-fpm-alpine
 }
 
 docker build -t sut .
 
-start_support_container
+start_php_container
 dgoss run --link test_support_php:php -p 8080:80 sut
 dgoss run --link test_support_php:php -e VIRTUAL_HOST=foo.bar -p 8080:80 sut
 dgoss run --link test_support_php:php -e VIRTUAL_HOST=localhost:8080 -p 8080:80 sut
-stop_support_container
+stop_php_container
