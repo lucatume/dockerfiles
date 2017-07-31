70f03a2c62b6f734665a20f105e0145c8d05de14
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-07-31 15:35:34 +0200

    added Nginx container tests

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -13,13 +13,13 @@ function stop_php_container(){
 
 function start_php_container(){
     stop_php_container
-    docker run --name test_support_php --rm -d php:7.1-fpm-alpine
+    docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
 }
 
 docker build -t sut .
 
 start_php_container
-dgoss run --link test_support_php:php -p 8080:80 sut
-dgoss run --link test_support_php:php -e VIRTUAL_HOST=foo.bar -p 8080:80 sut
-dgoss run --link test_support_php:php -e VIRTUAL_HOST=localhost:8080 -p 8080:80 sut
+dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -p 8080:80 sut
+dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -e VIRTUAL_HOST=foo.bar -p 8080:80 sut
+dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -e VIRTUAL_HOST=localhost:8080 -p 8080:80 sut
 stop_php_container
