8d799383e742c165a0ec723f6b0a1dbcd0a30ee6
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-01 22:04:00 +0200

    remove conditional alltogether

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -4,10 +4,7 @@
 docker pull php:7.1-fpm-alpine
 
 # spin up the required PHP container
-running="$(docker ps -aq --filter="name=test_support_php")"
-if [ "$running" == "" ]; then
-	docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
-fi
+docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
 
 docker build -t nginx .
 
