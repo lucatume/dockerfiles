3a131e46a5c9e14bdab1a795f267ea20b5b0be7a
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-01 20:01:20 +0200

    rewrite nginx container support functions

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -4,11 +4,17 @@
 docker pull php:7.1-fpm-alpine
 
 # spin up the required PHP container
-running="$(docker ps -aq --filter="name=test_support_php")"
-if [[ $running != "" ]]; then
-    docker stop $running
-fi
-docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
+function stop_php_container {
+	running="$(docker ps -aq --filter="name=test_support_php")"
+	if [[ $running != "" ]]; then
+	    docker stop $running
+	fi
+}
+
+function start_php_container {
+	stop_php_container
+	docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
+}
 
 docker build -t nginx .
 
