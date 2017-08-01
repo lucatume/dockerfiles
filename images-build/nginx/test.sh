8bc6e7d10451f465d00740df66cc1b3c3558157e
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-01 20:41:22 +0200

    simplify support php container checks

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -4,22 +4,13 @@
 docker pull php:7.1-fpm-alpine
 
 # spin up the required PHP container
-function stop_php_container {
-	running="$(docker ps -aq --filter="name=test_support_php")"
-	if [[ $running != "" ]]; then
-	    docker stop $running
-	fi
-}
-
-function start_php_container {
-	stop_php_container
+running="$(docker ps -aq --filter="name=test_support_php")"
+if [[ $running == "" ]]; then
 	docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
-}
+fi
 
 docker build -t nginx .
 
-start_php_container
 dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -p 8080:80 nginx 
 dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -e VIRTUAL_HOST=foo.bar -p 8080:80 nginx 
 dgoss run --link test_support_php:php -v $(pwd)/tests/app:/var/www/html -e VIRTUAL_HOST=localhost:8080 -p 8080:80 nginx 
-stop_php_container
