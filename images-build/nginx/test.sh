eca082f0652125649cc73131a1ba425267376b31
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-01 19:30:46 +0200

    fix function syntax

--- images-build/nginx/test.sh
+++ images-build/nginx/test.sh
@@ -4,14 +4,14 @@
 docker pull php:7.1-fpm-alpine
 
 # spin up the required PHP container
-function stop_php_container(){
+function stop_php_container{
     running="$(docker ps -aq --filter="name=test_support_php")"
     if [[ $running != "" ]]; then
         docker stop $running
     fi
 }
 
-function start_php_container(){
+function start_php_container{
     stop_php_container
     docker run --name test_support_php -v $(pwd)/tests/app:/var/www/html --rm -d php:7.1-fpm-alpine
 }
