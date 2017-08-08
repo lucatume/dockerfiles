dd3d30e8ffd207171d35ce1e84c8e429bd008f32
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-08 17:32:27 +0200

    remove docker tagging

--- images-build/php-52-xdebug/test.sh
+++ images-build/php-52-xdebug/test.sh
@@ -12,5 +12,3 @@ dgoss run -e XDEBUG_DISABLE=1-e XDEBUG_REMOTE_PORT=8877 lucatume/php-52-xdebug
 dgoss run -e XDEBUG_DISABLE=1 -e XDEBUG_REMOTE_PORT=8877 -e XDEBUG_REMOTE_HOST=1.2.3.4 lucatume/php-52-xdebug 
 dgoss run -e OPCACHE_DISABLE=1 lucatume/php-52-xdebug 
 dgoss run -e MEMCACHE_DISABLE=1 lucatume/php-52-xdebug 
-
-docker tag lucatume/php-52-xdebug lucatume/php-52-xdebug:latest
--- images-build/test-front/test.sh
+++ images-build/test-front/test.sh
@@ -11,5 +11,3 @@ dgoss run -it -e XDEBUG_DISABLE=1-e XDEBUG_REMOTE_HOST=1.2.3.4 lucatume/test-fro
 dgoss run -it -e XDEBUG_DISABLE=1-e XDEBUG_REMOTE_PORT=8877 lucatume/test-front bash
 dgoss run -it -e XDEBUG_DISABLE=1 -e XDEBUG_REMOTE_PORT=8877 -e XDEBUG_REMOTE_HOST=1.2.3.4 lucatume/test-front bash
 dgoss run -it -e OPCACHE_DISABLE=1 lucatume/test-front bash
-
-docker tag lucatume/test-front lucatume/test-front:latest
