bc49018c0f403dd0e6ff0789a2b178888110a600
Luca Tumedei <luca@theaveragedev.com>
2020-04-09 14:09:06 +0200

    feat(containers) support FIXUID env var to toggle fixuid on and off in codeception and composer containers

--- containers/composer/docker-entrypoint.sh
+++ containers/composer/docker-entrypoint.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ "${UID}:${GID}" != "0:0" ]]; then
-    echo -e "\033[32mFixing file ownership issues...\033[0m"
-    # Use fixuid to remap UID and GID correctly in the /composer folders.
-    eval $(fixuid)
-else
-    echo 'If you are encountering file ownership issues, run this container using the "--user ${UID}:${GID}" option.'
-    echo 'The issues should be fixed for you.'
-fi
+function fix_uid(){
+		if [[ "${UID}:${GID}" != "0:0" ]]; then
+				echo -e "\033[32mFixing file ownership issues...\033[0m"
+				# Use fixuid to remap UID and GID correctly in the /composer folders.
+				eval "$(fixuid)"
+		else
+				echo "If you are encountering file ownership issues, run this container using the '--user ${UID}:${GID}' option."
+				echo "The issues should be fixed for you."
+		fi
+}
 
-COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_HOME=/composer COMPOSER_CACHE=/composer/cache composer $@
+test "${FIXUID:-1}" && fix_uid
+
+COMPOSER_ALLOW_SUPERUSER=1 COMPOSER_HOME=/composer COMPOSER_CACHE=/composer/cache composer "$@"
