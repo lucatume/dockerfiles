75523b27a3e984fe0e19061fcea3afab9dd46bcb
Luca Tumedei <luca@theaveragedev.com>
2020-11-11 11:40:38 +0100

    fix(containers/composer) quiet fixuid output

--- containers/composer/docker-entrypoint.sh
+++ containers/composer/docker-entrypoint.sh
@@ -2,9 +2,10 @@
 
 function fix_uid(){
 		if [[ "${UID}:${GID}" != "0:0" ]]; then
-				echo -e "\033[32mFixing file ownership issues...\033[0m"
+				echo -n "Fixing file ownership issues ..."
 				# Use fixuid to remap UID and GID correctly in the /composer folders.
-				eval "$(fixuid)"
+				eval "$(fixuid -q)"
+				echo -e " \e[32mdone\e[0m"
 		else
 				echo "If you are encountering file ownership issues, run this container using the '--user ${UID}:${GID}' option."
 				echo "The issues should be fixed for you."
